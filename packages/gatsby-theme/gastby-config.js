ca680728fad104366db125dd3bf7e3fc1ff1dfd1
Ekwuno Obinna <obinnacodes@gmail.com>
2019-02-27 14:24:57 +0100

    gatsby-config.js

---
+++ packages/gatsby-theme/gastby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    plugins:[
+       {
+        resolve:'gatsby-mdx',
+        options :{
+            
+        }
+       },
+       {
+        resolve: `gatsby-plugin-page-creator`,
+        options: {
+          path: `${__dirname}/src/src/pages`,
+        },
+       }
+
+    ]
+}
